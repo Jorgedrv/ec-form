50397c642fe28b3ea4693bd75dd6f5bb9bfeac73
Jorge <jorgeddrv@gmail.com>
2023-08-07 13:57:54 -0500

    Several fixes were added

--- src/componets/footer/Footer.jsx
+++ src/componets/footer/Footer.jsx
@@ -6,17 +6,17 @@ const Footer = () => {
     return (
         <footer className="footer text-center">
             <div className="social-container">
-                <a href="https://www.youtube.com/c/jamesqquick" target="_blank" rel="noopener noreferrer" className="youtube social">
+                <a href="https://www.youtube.com/@esperanzachurch5240" target="_blank" rel="noopener noreferrer" className="youtube social">
                     <FontAwesomeIcon icon={faYoutube} size="2x" />
                 </a>
-                <a href="https://www.youtube.com/c/jamesqquick" target="_blank" rel="noopener noreferrer" className="facebook social">
+                <a href="https://www.facebook.com/EsperanzaChurchSpringHill" target="_blank" rel="noopener noreferrer" className="facebook social">
                     <FontAwesomeIcon icon={faFacebook} size="2x" />
                 </a>
-                <a href="https://www.instagram.com/esperanza_church/" target="_blank" rel="noopener noreferrer" className="instragam social">
+                <a href="https://www.instagram.com/esperanza_church" target="_blank" rel="noopener noreferrer" className="instragam social">
                     <FontAwesomeIcon icon={faInstagram} size="2x" />
                 </a>
             </div>
-            <div className="text-black text-center p-4" style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}>
+            <div className="text-black text-center p-4" style={{ backgroundColor: "white" }}>
                 &copy; {new Date().getFullYear()} Copyright:{" "}
                 <a className="text-reset fw-bold" href="/">
                     Esperanza Church
