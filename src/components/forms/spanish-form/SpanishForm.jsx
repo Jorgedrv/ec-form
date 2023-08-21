12154dbb12314b332a7342292c4f72d4c8d22002
Jorge <jorgeddrv@gmail.com>
2023-08-21 18:43:33 -0500

    Image responsive

--- src/components/forms/spanish-form/SpanishForm.jsx
+++ src/components/forms/spanish-form/SpanishForm.jsx
@@ -7,7 +7,7 @@ const EnglishForm = () => {
                 <h1 className="display-4 centered">Clases de Español</h1>
             </div>
             <div className="text-center">
-                <img width={980} height={520} src="/images/under-construction.jpg" alt="under" className="mx-auto d-block img-fluid" />
+                <img src="/images/under-construction.jpg" alt="under" className="mx-auto d-block img-fluid" />
                 <p className="display-6 centered">Under Construction...</p>
             </div>
         </Container>
